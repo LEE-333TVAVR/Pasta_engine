7d52109536e60bbe0bd0d87568855ec82cdb3aac
miiim46 <tizinforte6@gmail.com>
2026-01-23 18:50:40 +0600

    Каркас 0.2

---
+++ Main_test.pas
@@ -0,0 +1,16 @@
+﻿
+uses Main, GraphABC;
+begin
+  var Гоблин:= new Picture('Гоблин.png');
+  var Массив_объектов : array of Каннеллони;
+  SetLength(Массив_объектов,1);
+  Массив_объектов[0]:= new Каннеллони(60,60,320,320,Гоблин);
+  var Камера_1:Сцена;
+  Камера_1:= new Сцена(Массив_объектов,true,true,640,640);
+  Камера_1.Update;
+  while true do
+  begin
+    Sleep(10);
+    Камера_1.Update;
+  end;
+end.
